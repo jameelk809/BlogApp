5986174464de00801abc9fe47d17d6c458e6fecb
Kamran Jameel <jameelk809@gmail.com>
2022-09-25 23:59:59 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+// components
+import Login from './components/account/login';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React
-        </a>
-      </header>
+      <Login />
     </div>
   );
 }
---
+++ src/components/account/login.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Login = () => {
+    return (
+        <p>
+            Hello from Login
+        </p>
+    )
+}
+
+export default Login;
