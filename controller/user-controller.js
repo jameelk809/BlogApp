a16a2ff2c4759813c3211fe0b57a98e9f09c7e9f
Kamran Jameel <jameelk809@gmail.com>
2022-09-30 23:55:16 +0530

    created API

---
+++ controller/user-controller.js
@@ -0,0 +1,13 @@
+
+import User from "../model/user.js";
+
+export const signupUser = async (request, response) => {
+    try {
+        const user = request.body;
+        const newUser = new User(user);
+        await newUser.save();
+        return response.status(200).json({ msg: 'signup successful' })
+    } catch (error) {
+        return response.status(500).json({ msg: 'Error while Signup' })
+    }
+}
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 
 
-import Connection from "./database/db.js";
+import Connection from './database/db.js';
+import Router from './routes/route.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use('/', Router);
 
 
 const PORT = 8000;
