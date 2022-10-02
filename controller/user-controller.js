6edeac14aef1672983f59190169779849b92549b
Kamran Jameel <jameelk809@gmail.com>
2022-10-02 20:27:17 +0530

    JWT authenticated

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 import User from "../model/user.js";
+import token from "../model/token.js";
 
 dotenv.config();
 
@@ -22,5 +23,26 @@ export const signupUser = async (request, response) => {
 
 
 export const loginUser = async (request, response) => {
+    let user = await User.findOne({ username: request.body.username });
+    if (!user) {
+        return response.status(400).json({ msg: 'Username does not match' });
+    }
 
+    try {
+        let match = await bcrypt.compare(request.body.password, user.password);
+        if (match) {
+            const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m'});
+            const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
+            
+            const newToken = new token({ token: refreshToken });
+            await newToken.save();
+        
+            return response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken, name: user.name, username: user.username });
+        
+        } else {
+            return response.status(400).json({ msg: 'Password does not match' })
+        }
+    } catch (error) {
+        response.status(500).json({ msg: 'error while login the user' })
+    }
 }
