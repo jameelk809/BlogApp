c405acd7dc9859b3f97c5f1fcc0571f343a6b3e9
Kamran Jameel <jameelk809@gmail.com>
2022-10-02 15:04:43 +0530

    cors installed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 
 import Connection from './database/db.js';
@@ -9,6 +10,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use('/', Router);
 
 
