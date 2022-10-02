01096af6518645bd704099f1df9011eca7a77d6c
Kamran Jameel <jameelk809@gmail.com>
2022-10-02 15:54:41 +0530

    bcrypt implemented

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,9 +1,12 @@
-
+import bcrypt from 'bcrypt';
 import User from "../model/user.js";
 
 export const signupUser = async (request, response) => {
     try {
-        const user = request.body;
+        // const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(request.body.password, 10);
+
+        const user = { username: request.body.username, name: request.body.name, password: hashedPassword };
         const newUser = new User(user);
         await newUser.save();
         return response.status(200).json({ msg: 'signup successful' })
