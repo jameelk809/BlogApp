2012ac5a183a59e7ad05ddcb81c9efc1cc5c2087
Kamran Jameel <jameelk809@gmail.com>
2023-01-06 17:26:00 +0530

    Categories table styled

--- src/components/home/Categories.jsx
+++ src/components/home/Categories.jsx
@@ -1,12 +1,23 @@
 
-import { Button, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { Button, Table, TableBody, TableCell, TableHead, TableRow, styled } from "@mui/material";
 import { categories } from "../../constants/data";
+
+const StyledTable = styled(Table)`
+    border: 1px solid rgba(224, 224, 224, 1);
+`;
+
+const StyledButton = styled(Button)`
+    margin: 20px;
+    width: 85%;
+    background: #6495ED;
+    color: #fff;
+`
 
 const Categories = () => {
     return(
         <>
-            <Button>Create Blog</Button>
-            <Table>
+            <StyledButton variant="contained">Create Blog</StyledButton>
+            <StyledTable>
                 <TableHead>
                     <TableRow>
                         <TableCell>
@@ -23,7 +34,7 @@ const Categories = () => {
                         ))
                     }
                 </TableBody>
-            </Table>
+            </StyledTable>
         </>
     )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 
 
 // components
+import { Grid } from "@mui/material";
 import Banner from "../banner/Banner";
 import Categories from "./Categories";
 
@@ -8,7 +9,14 @@ const Home = () => {
     return (
         <>
             <Banner />
-            <Categories />
+            <Grid container>
+                <Grid item lg={2} sm={2} xs={12}>
+                    <Categories />
+                </Grid>
+                <Grid container item xs={12} sm={10} lg={10}>
+                    Posts
+                </Grid>
+            </Grid>
         </>
     )
 }
